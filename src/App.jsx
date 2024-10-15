a873685b15291d008030cb575956962fda4dfc51
ajit-divakaran <ajit.divakaranb@gmail.com>
2024-10-15 19:41:11 +0530

    Made an initial project file and entered name Ajit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <>
+      <div className="bg-dark w-full row">
+        <div className="d-none d-md-block col-md-2 bg-secondary vh-100"></div>
+        <div className="col-12 col-md-8 vh-100 d-flex justify-content-center align-items-center">
+          <div>
+            <p className="text-light">Add your name to the list</p>
+            <ul className="list-unstyled">
+              <li className="text-light">Hello My name is <span style={{color:'yellow'}}>Ajit</span></li>
+            </ul>
+          </div>
+        </div>
+        <div className="d-none d-md-block col-md-2 bg-secondary vh-100"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
